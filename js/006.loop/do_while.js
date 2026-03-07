93dbf1b2237a224d8b71981f6bdd576c398a80aa
priya <priyamishra46839@gmail.com>
2026-03-07 10:11:41 +0530

    task

---
+++ js/006.loop/do_while.js
@@ -0,0 +1,15 @@
+// for (let i = 10; i < 10; i++) {
+//   console.log("-------->", i);
+// }
+
+/*
+fun1() -> get student data
+fun2() -> find student who are failed in exam
+fun3() -> send the mail to parent of failed student 
+*/
+
+let i = 9;
+do {
+  console.log("-------->", i);
+  i++
+} while (i < 10);
---
+++ js/006.loop/task.js
@@ -0,0 +1,31 @@
+// let str = "Hello Urvish, How are you ?";
+let str = "Hello Urvish ";
+console.log("-----as--->",str.charCodeAt(1))
+// let x = "qwe ";
+// console.log("🚀 ~ x:", x.length);
+
+// console.log("🚀 ~ x[0]:", x[2]);
+let totalSpace = 0;
+for (let i = 0; i < str.length; i++) {
+  console.log("----ch---->", str[i]);
+  //   totalSpace = totalSpace + 1;
+  if (str[i] === "U") {
+    totalSpace += 1;
+  }
+}
+console.log("🚀 ~ totalSpace:", totalSpace);
+
+
+/*
+Task :
+string : "How are you ?"
+charactor to count : "U"
+
+if charactor not available -> charactor not available ?
+
+Task :
+
+Charactor is uppercase
+Charactor is lowercase
+
+*/
