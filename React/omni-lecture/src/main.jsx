8257de3f2fae5f12dea412792bc986eeae19855b
priya <priyamishra46839@gmail.com>
2026-05-16 09:07:44 +0530

    react

--- React/omni-lecture/src/main.jsx
+++ React/omni-lecture/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<App/>);
+
+
+
+// let root = createRoot(document.getElementById("root"));
+
+// root.render(<h1>test</h1>);
